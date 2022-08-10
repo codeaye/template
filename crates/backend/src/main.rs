9d19a2489f7ffe171922e3519e503525a5c38f72
Nav <62173713+codeaye@users.noreply.github.com>
2022-08-10 14:31:47 +0100

    Add react-devtools

--- crates/backend/src/main.rs
+++ crates/backend/src/main.rs
@@ -3,9 +3,19 @@
     windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_window_state::Builder::default().build())
+        .setup(|app| {
+            #[cfg(debug_assertions)]
+            {
+                let window = app.get_window("main").unwrap();
+                window.open_devtools();
+            }
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("There was an error while running tauri application");
 }
